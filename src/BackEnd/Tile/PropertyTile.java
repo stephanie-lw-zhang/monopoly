b96f8678541be8eaafa301efec0e2dadd981fdca
Luis Fornes <laf30@duke.edu>
2019-04-03 16:06:11 -0400

    implemented 3 tile classes, abstract, property, and drawcard

---
+++ src/BackEnd/Tile/PropertyTile.java
@@ -0,0 +1,18 @@
+package BackEnd.Tile;
+
+import java.awt.*;
+
+public class PropertyTile extends AbstractTile{
+    private String tiletype;
+    private Color tilecolor;
+    private int tilerent;
+    private int tilerentwithcolorset;
+    private int tilerent1house;
+    private int tilerent2house;
+    private int tilerent3house;
+    private int tilerenthotel;
+    private int tilemortgagevalue;
+    private int tilehouseprice;
+    private int tilehotelprice;
+
+}
