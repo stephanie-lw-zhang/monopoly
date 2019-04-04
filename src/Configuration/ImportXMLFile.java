3c9c7839f9108936d68d14f926be79cbf22ed5e5
Luis Fornes <laf30@duke.edu>
2019-04-03 22:41:18 -0400

    added xml fileparser

---
+++ src/Configuration/ImportXMLFile.java
@@ -0,0 +1,55 @@
+package Configuration;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.ByteArrayInputStream;
+
+
+
+public class ImportXMLFile {
+
+    public static void main(String argv[]) {
+
+        try {
+
+            File xmlFile = new File("/Users/luisfornes/IdeaProjects/monopoly_team05/properties/OriginalMonopoly.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+
+            //optional, but recommended
+            //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("Tile");
+
+            System.out.println("----------------------------");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                Node nNode = nList.item(temp);
+
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("Tile Type : " + eElement.getAttribute("TileType"));
+                    System.out.println("TileNumber : " + eElement.getElementsByTagName("TileNumber").item(0).getTextContent());
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
