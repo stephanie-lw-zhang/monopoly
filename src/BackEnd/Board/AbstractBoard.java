15f1ebb8a6ebc981181aa24a142c7ebe0f78633b
eh158 <edward.hsieh2@duke.edu>
2019-04-05 20:47:49 -0400

    started barebones sketching

--- src/BackEnd/Board/AbstractBoard.java
+++ src/BackEnd/Board/AbstractBoard.java
@@ -1,5 +1,7 @@
 package BackEnd.Board;
 
+import BackEnd.Player.AbstractPlayer;
+
 /**
  * This class is an abstraction of the game board which contains
  * fundamental pieces to the game itself
@@ -7,4 +9,11 @@ package BackEnd.Board;
  * @author Sam
  */
 public abstract class AbstractBoard {
+
+    private void calculatePath(AbstractPlayer player){
+        var moves = myDice.roll();
+        for(int i=0;i<moves;i++){
+            
+        }
+    }
 }
