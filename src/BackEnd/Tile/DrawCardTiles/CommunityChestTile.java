949476838abeb5ca3a6e659d412e882f74c217d3
Luis Fornes <laf30@duke.edu>
2019-04-16 16:35:03 -0400

    implementing constructor for buildingTile that will allow XML data to be loaded into tile objects

--- src/BackEnd/Tile/DrawCardTiles/CommunityChestTile.java
+++ src/BackEnd/Tile/DrawCardTiles/CommunityChestTile.java
@@ -1,11 +1,16 @@
 package BackEnd.Tile.DrawCardTiles;
 
 import BackEnd.Deck.DeckInterface;
+import org.w3c.dom.Element;
 
 public class CommunityChestTile extends AbstractDrawCardTile {
 
     public CommunityChestTile(DeckInterface deck) {
         super(deck);
+    }
+
+    public CommunityChestTile(Element n){
+        super(n);
     }
 
 }
