81e1b9ab82642f9501ebcdf9c8f4313a1533913c
Luis Fornes <laf30@duke.edu>
2019-04-16 22:45:37 -0400

    beginning pop up imoplementation for tiles on board

--- src/BackEnd/Tile/FreeParkingTile.java
+++ src/BackEnd/Tile/FreeParkingTile.java
@@ -1,10 +1,7 @@
 package BackEnd.Tile;
 
 import BackEnd.AssetHolder.AbstractPlayer;
-<<<<<<< HEAD
-=======
-//import api.Monopoly.BackEnd.AbstractTile;
->>>>>>> 0d3378c11f6e47b4fccb9f2af6e064ee1f79ebf2
+
 import org.w3c.dom.Element;
 
 import java.util.ArrayList;
