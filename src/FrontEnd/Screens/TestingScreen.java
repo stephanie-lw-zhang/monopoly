b177cacaf1c5436eb222d13a99251f07adb0c7e7
eh158 <edward.hsieh2@duke.edu>
2019-04-17 14:58:05 -0400

    changed dice appearance

--- src/FrontEnd/Screens/TestingScreen.java
+++ src/FrontEnd/Screens/TestingScreen.java
@@ -172,7 +172,7 @@ public class TestingScreen extends AbstractScreen {
         playerOptionsModal.setSpacing(10);
 
         HBox diceLayout = new HBox();
-        diceLayout.setSpacing(10);
+        diceLayout.setSpacing(20);
         diceLayout.setUserData("diceLayout");
 
         ImageView dice1 = new ImageView();
@@ -183,8 +183,8 @@ public class TestingScreen extends AbstractScreen {
                         "dice" + (new Random().nextInt(myGame.getMyDice().getNumStates()) + 1) + ".png"
                 )
         ));
-        dice1.setFitHeight(30);
-        dice1.setFitWidth(30);
+        dice1.setFitHeight(55);
+        dice1.setFitWidth(55);
 
         ImageView dice2 = new ImageView();
         dice2.setImage(new Image(this
@@ -194,8 +194,8 @@ public class TestingScreen extends AbstractScreen {
                         "dice" + (new Random().nextInt(myGame.getMyDice().getNumStates()) + 1) + ".png"
                 )
         ));
-        dice2.setFitHeight(30);
-        dice2.setFitWidth(30);
+        dice2.setFitHeight(55);
+        dice2.setFitWidth(55);
 
         diceLayout.getChildren().addAll(dice1, dice2);
         diceLayout.setAlignment(Pos.CENTER_RIGHT);
