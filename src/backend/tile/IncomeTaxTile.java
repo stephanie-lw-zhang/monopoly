d107b4dc3a237573eafa338472c6ba37a9afa521
Connie Wu <connie.wu@duke.edu>
2019-04-20 14:11:32 -0400

    Add new file

---
+++ src/backend/tile/IncomeTaxTile.java
@@ -0,0 +1,34 @@
+package backend.tile;
+
+import backend.assetholder.AbstractPlayer;
+import backend.assetholder.Bank;
+import org.w3c.dom.Element;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IncomeTaxTile extends AbstractTaxTile {
+
+    public IncomeTaxTile(int money, Bank bank, int index) {
+        super(money, bank, index);
+    }
+
+    public IncomeTaxTile(Bank bank, Element n){
+        super(bank, n);
+    }
+
+    @Override
+    public List<String> applyLandedOnAction(AbstractPlayer player) {
+        //interaction with front-end: pay full or 10%?
+        List<String> possibleActions = new ArrayList<>(  );
+        possibleActions.add("payTaxFixed");
+        possibleActions.add("payTaxPercentage");
+//        if (true) {
+//            player.paysTo(getBank(),getAmountToDeduct());
+//        }
+//        else {
+//            player.paysTo(getBank(), player.getMoney() * 0.1);
+//        }
+        return possibleActions;
+    }
+}
