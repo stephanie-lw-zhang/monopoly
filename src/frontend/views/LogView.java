789bcd62e8997603abc01faa6c456d413cdfb4b4
syz5@duke.edu <samzyao@gmail.com>
2019-04-26 20:12:25 -0400

    doc for LogView

--- src/frontend/views/LogView.java
+++ src/frontend/views/LogView.java
@@ -4,27 +4,38 @@ import configuration.XMLData;
 import javafx.scene.Node;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.Pane;
+
+/**
+ * This class represents the View for all history
+ * messages that are logged in the view area
+ *
+ * @author Luis
+ * @author Sam
+ *              [updated constructor & log to append not replace
+ */
 public class LogView {
 
     private XMLData myData;
     private TextArea myGameLog;
     private StringBuilder myLogContent;
 
-    private StackPane myRoot;
-
+    /**
+     * LogView main constructor
+     * @param data
+     */
     public LogView(XMLData data) {
         myData = data;
-        myRoot = new StackPane();
         myLogContent = new StringBuilder("Welcome to " + myData.getMonopolyType() + ".");
-        myLogContent.append("\nhello");
-        myLogContent.append("\nok");
-        myLogContent.append("\nsweet");
         myGameLog = new TextArea(myLogContent.toString());
         myGameLog.setEditable(false);
         myGameLog.setStyle("-fx-max-width: 400; -fx-max-height: 100");
     }
 
+    /**
+     * Updates the LogView area with the given message
+     * that is appended to the LogView area
+     * @param message       the text to be logged
+     */
     public void updateLogDisplay(final String message) {
         myLogContent.append("\n");
         myLogContent.append(message);
@@ -32,8 +43,8 @@ public class LogView {
     }
 
     /**
-     *
-     * @return
+     * Getter for the Node of a LogView
+     * @return Node         TextArea
      */
     public Node getNode() {
         return myGameLog;
